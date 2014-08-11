54b9fcc3129056236d7bbb0c4ade227183b1e14a
Aaron Wong <akwong@princeton.edu>
2014-08-11 10:46:29 -0400

    Added category filtering

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ wget -P download/ -N https://pharmgkb-owl.googlecode.com/files/mesh.owl.gz
 gunzip -c download/mesh.owl.gz > download/mesh.owl
 
 python utils/make_mesh_mapping.py -x download/mesh.owl -o meshid_map.txt
+
+python utils/make_mesh_mapping.py -x download/mesh.owl -o meshid_anatomy_map.txt -c A
+python utils/make_mesh_mapping.py -x download/mesh.owl -o meshid_disease_map.txt -c C
+
+bash retrieve.sh meshid_anatomy_map.txt output-anatomy
+bash retrieve.sh meshid_disease_map.txt output-disease
--- utils/make_mesh_mapping.py
+++ utils/make_mesh_mapping.py
@@ -11,12 +11,12 @@ parser = OptionParser(usage, version="%prog dev-unreleased")
 
 parser.add_option("-x", "--mesh-xml", dest="mesh_xml", help="MeSH OWL XML file", metavar="FILE")
 parser.add_option("-o", "--output-file", dest="output", help="Output file", metavar="FILE")
+parser.add_option("-c", "--mesh-prefix", dest="mesh_pref", help="MeSH Category Prefix", default='')
 
 (options, args) = parser.parse_args()
 
 NAMESPACE = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}'
 
-ofile = open(options.output, 'w')
 
 
 try:
@@ -26,13 +26,23 @@ except ET.ParseError:
 
 root = meta.getroot()
 
+
+meshids = {}
+meshtree = {}
 for child in root.findall('{http://www.w3.org/2002/07/owl#}Class'):
     #print child.tag, child.attrib, child.text
     name = child.find('{http://www.w3.org/2000/01/rdf-schema#}label').text
     meshid = child.get(NAMESPACE+'about')
-    meshid = meshid.split('#')[1] 
-    if meshid.startswith('D') and meshid.find('.') == -1:
-        ofile.write( name + '\t' + meshid + '\n' )
+    meshid = meshid.split('#')[1]
+    if meshid.find('.') == -1 and len(meshid) == 7:
+        #ofile.write( name + '\t' + meshid + '\n' )
+        meshids[name] = meshid
+    elif meshid.startswith(options.mesh_pref):
+        meshtree[name] = meshid
 
+
+ofile = open(options.output, 'w')
+for (name,mtree) in meshtree.iteritems():
+    ofile.write(name + '\t' + (meshids[name] if name in meshids else '') + '\t' + mtree + '\n')
 ofile.close()
 
